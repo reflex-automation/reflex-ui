362c670ce9a067236bef000375e90672acd3658c
Sam McCarthy <samuel.mccarthy@pm.me>
2026-08-11 12:23:54 +0100

    show sso login options from ui_auth on eda login page

--- frontend/eda/main/EdaLogin.tsx
+++ frontend/eda/main/EdaLogin.tsx
@@ -1,11 +1,19 @@
 import { LoadingState } from '@ansible/ansible-ui-framework/components/LoadingState';
 import { AnsibleLogin } from '@ansible/common-ui/AnsibleLogin/AnsibleLogin';
-import { mutate } from 'swr';
+import type { AuthOption } from '@ansible/common-ui/SocialAuthLogin';
+import { requestGet } from '@ansible/common-ui/crud/Data';
+import useSWR, { mutate } from 'swr';
 import { edaAPI } from '../common/eda-utils';
 import { useEdaActiveUser } from '../common/useEdaActiveUser';
 
-export function EdaLogin(props: { children: React.ReactNode }) {
+type EdaUIAuth = {
+  ssos?: AuthOption[];
+  show_login_form?: boolean;
+};
+
+export function EdaLogin(props: Readonly<{ children: React.ReactNode }>) {
   const { activeEdaUser, refreshActiveEdaUser } = useEdaActiveUser();
+  const { data: uiAuth } = useSWR<EdaUIAuth>(edaAPI`/ui_auth/`, requestGet);
 
   if (activeEdaUser === undefined) {
     return <LoadingState />;
@@ -15,13 +23,14 @@ export function EdaLogin(props: { children: React.ReactNode }) {
     return (
       <AnsibleLogin
         loginApiUrl={edaAPI`/auth/session/login/`}
+        authOptions={uiAuth?.ssos}
         onSuccess={() => {
           refreshActiveEdaUser?.();
           void mutate(() => true);
         }}
         brandImg="/reflex-logo.svg"
         brandImgAlt={process.env.PRODUCT as unknown as string}
-        showLoginForm={true}
+        showLoginForm={uiAuth?.show_login_form ?? true}
       />
     );
   }
